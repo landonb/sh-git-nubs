31dc395d8db0cbdbbbb3666ae1a234c8b6aa189c
Landon Bouma <landonb@users.noreply.github.com>
2024-08-26 13:17:09 -0500

    Feature: Identify recent tags: new git_most_recent_tag, git_most_recent_version_tag

--- lib/git-nubs.sh
+++ lib/git-nubs.sh
@@ -241,6 +241,22 @@ git_branches_with_tag () {
   git branch --list --contains refs/tags/${tag_name} $@
 }
 
+# CALSO: git_most_recent_tag / git_most_recent_version_tag
+git_most_recent_tag () {
+  local gitref="$1"
+
+  local contains=""
+  if [ -n "${gitref}" ]; then
+    # This adds a ~<n> suffix to the tag name, e.g., if the tag
+    # named 'foo' is 5 commits away from gitref, prints "foo~5".
+    contains="--contains ${gitref}"
+  fi
+
+  git describe --tags --abbrev=0 ${contains} \
+    2> /dev/null \
+    | sed 's/\~.*//'
+}
+
 # ***
 
 git_HEAD_commit_sha () {
@@ -803,6 +819,8 @@ GITNUBS_VERSION_TAG_PATTERNS="${GITNUBS_PREFIX}[0-9]* [0-9]*"
 
 GITNUBS_TAG_PATTERNS_TAGREFS="refs/tags/${GITNUBS_PREFIX}[0-9]* refs/tags/[0-9]*"
 
+GITNUBS_DESCRIBE_MATCH_PATTERNS="--match ${GITNUBS_PREFIX}[0-9]* --match [0-9]*"
+
 # Prints all tags that match: v[0-9]* [0-9]*
 _git_tag_list_prefilter () {
   git tag -l "$@" ${GITNUBS_VERSION_TAG_PATTERNS}
@@ -1098,6 +1116,22 @@ git_largest_version_tag_from_remote_normal () {
 }
 
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+
+# CALSO: git_most_recent_tag / git_most_recent_version_tag
+git_most_recent_version_tag () {
+  local gitref="$1"
+
+  local contains=""
+  if [ -n "${gitref}" ]; then
+    # This adds a ~<n> suffix to the tag name, e.g., if the tag
+    # named 'foo' is 5 commits away from gitref, prints "foo~5".
+    contains="--contains ${gitref}"
+  fi
+
+  git describe --tags --abbrev=0 ${contains} ${GITNUBS_DESCRIBE_MATCH_PATTERNS} \
+    2> /dev/null \
+    | sed 's/\~.*//'
+}
 
 git_since_most_recent_commit_epoch_ts () {
   git --no-pager log -1 --format=%at HEAD 2> /dev/null
