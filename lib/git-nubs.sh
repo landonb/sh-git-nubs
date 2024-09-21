97089ff49350376d63c5af44d7c486a94a5d1643
Landon Bouma <landonb@users.noreply.github.com>
2024-09-20 19:27:02 -0500

    Feature: New fcn, git_tree_object_name, like git_commit_object_name

--- lib/git-nubs.sh
+++ lib/git-nubs.sh
@@ -102,10 +102,26 @@ git_tracking_branch_safe () {
 #        it without checking if object actually exists.
 #        - See git_is_commit for checking if commit object.
 git_commit_object_name () {
-  local gitref="${1:-HEAD}"
-  local opts="$2"
+  git_typed_object_name "commit" "$@"
+}
 
-  git rev-parse ${opts} --verify --end-of-options "${gitref}^{commit}" 2> /dev/null
+# UCASE: Check if tag is on magic empty tree object (4b825dc642cb), e.g.,
+#   $ git tag some-tag 4b825dc642cb  # Ever-present magic empty tree
+#   $ git_tree_object_name some-tag
+#   4b825dc642cb6eb9a060e54bf8d69288fbee4904
+git_tree_object_name () {
+  git_typed_object_name "tree" "$@"
+}
+
+git_typed_object_name () {
+  local type="$1"
+  local gitref="${2:-HEAD}"
+  local opts="$3"
+
+  # CPYST:
+  #   gitref=HEAD && git rev-parse --verify "${gitref}^{commit}"
+  #   gitref=refs/tags/1.2.3 && git rev-parse --verify "${gitref}^{commit}"
+  git rev-parse ${opts} --verify --end-of-options "${gitref}^{${type}}" 2> /dev/null
 }
 
 git_is_same_commit () {
